98a01039bb2ab57731e8294df0fd286fe40db0ef
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2022-12-03 19:52:33 +0800

    Continued working on the app, probably works now

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const velocity1 = document.getElementById('velocity1')
 const velocityAfter1 = document.getElementById('velocityAfter1')
 const velocity2 = document.getElementById('velocity2')
 const velocityAfter2 = document.getElementById('velocityAfter2')
+// Globally declare variables
+let ball1, ball2;
+let isRunning = false;
+
 
 class Ball {
     constructor(mass, velocity, velocityAfter, positionX) {
@@ -39,34 +43,28 @@ class Ball {
 
 }
 
-// Declare variables
-let ball1, ball2;
-let isRunning = false;
-
-const solveCollision = (mass1, mass2, v1, v2, u1, u2) => {
+const solveCollision = (mass1, mass2, v1, v2, u1, u2, missingTerm) => {
     // Formula: mass1 * mass2 *  
-    if (isNaN(mass1)) {
-        let equation = `x * ${v1} + (${mass2})(${v2}) = x * ${u1} + (${mass2})(${u2})`;
-        let sol = nerdamer.solveEquations(`x * ${v1} + (${mass2})(${v2}) = x * ${u1} + (${mass2})(${u2})`, 'x');
-        alert("Mass 1: " + sol.toString())
-        return parseFloat(sol.toString())
-    } else if (isNaN(mass2)) {
-        let sol = nerdamer.solveEquations(`x * ${v1} + (${mass2})(${v2}) = x * ${u1} + (${mass2})(${u2})`, 'x');
-        alert("Mass 2: " + sol.toString())
-        return parseFloat(sol.toString())
+    let sol;
+    if (missingTerm == "mass1") {
+        sol = nerdamer.solveEquations(`(x)(${v1}) + (${mass2})(${v2}) = (x)(${u1}) + (${mass2})(${u2})`, 'x')
+    } else if (missingTerm == "mass2") {
+        sol = nerdamer.solveEquations(`(${mass1})(${v1}) + (x)(${v2}) = (${mass1})(${u1}) + (x)(${u2})`, 'x');
     }
 
-    if (isNaN(v1)) {
-
-    } else if (isNaN(v2)) {
-
+    if (missingTerm == "velocity1") {
+        sol = nerdamer.solveEquations(`(${mass1})(x) + (${mass2})(${v2}) = (${mass1})(${u1}) + (${mass2})(${u2})`, 'x')
+    } else if (missingTerm == "velocity2") {
+        sol = nerdamer.solveEquations(`(${mass1})(${v1}) + (${mass2})(x) = (${mass1})(${u1}) + (${mass2})(${u2})`, 'x')
     }
 
-    if (isNaN(u1)) {
-
-    } else if (isNaN(u2)) {
-
+    if (missingTerm == "velocityAfter1") {
+        sol = nerdamer.solveEquations(`(${mass1})(${v1}) + (${mass2})(${v2}) = (${mass1})(x) + (${mass2})(${u2})`, 'x')
+    } else if (missingTerm == "velocityAfter2") {
+        sol = nerdamer.solveEquations(`(${mass1})(${v1}) + (${mass2})(${v2}) = (${mass1})(${u1}) + (${mass2})(x)`, 'x')
     }
+    console.log(parseFloat(eval(sol.toString()).toFixed(5)))
+    return parseFloat(eval(sol.toString()).toFixed(5))
 }
 
 const mainLoop = () => {
@@ -77,42 +75,125 @@ const mainLoop = () => {
     ball2.update()
     // Check for collision
     if ((ball1.size + ball2.size) >=  ball2.positionX - ball1.positionX) {
-        console.log(ball1.velocityAfter, ball2.velocityAfter)
         console.log("Collision detected!") 
         // Do the maths or physics I should say, need velocity after
-        // if (ball1.velocityAfter) {
-        //     console.log(`V1 = ${ball1.velocity}`)
-        //     ball1.velocity = ball1.velocityAfter;
-        // }
-        // if (ball2.velocityAfter) {
-        //     console.log(`V2 = ${ball2.velocity}`)
-        //     ball2.velocity = ball2.velocityAfter;
-        // }
+        ball1.velocity = ball1.velocityAfter;
+        ball2.velocity = ball2.velocityAfter;
     }
+
     // Render
     ball1.draw()
     ball2.draw()
+
+    // Check if animation is over
+    if (ball1.positionX + ball1.size <= 0 && ball2.positionX + ball2.size >= 800) {
+        console.log("Animation has stopped")
+        animationOver()
+        return;
+    }
     // Repeat loop
     requestAnimationFrame(mainLoop);
 }
+
+let animationOver = () => {
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(0, 0, 800, 400);
+    isRunning = false;
+}
+
+let generateBalls = (value, missingTerm) => {
+    // Start animation now
+    if (missingTerm == "mass1") {
+        ball1 = new Ball(value, parseFloat(velocity1.value), parseFloat(velocityAfter1.value), 100)
+        ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), parseFloat(velocityAfter2.value), 700)
+    } else if (missingTerm == "mass2") {
+        ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), parseFloat(velocityAfter1.value), 100)
+        ball2 = new Ball(value, parseFloat(velocity2.value), parseFloat(velocityAfter2.value), 700)
+    }
+
+    if (missingTerm == "velocity1") {
+        ball1 = new Ball(parseFloat(mass1.value), value, parseFloat(velocityAfter1.value), 100)
+        ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), parseFloat(velocityAfter2.value), 700)
+    } else if (missingTerm == "velocity2") {
+        ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), parseFloat(velocityAfter1.value), 100)
+        ball2 = new Ball(parseFloat(mass2.value), value, parseFloat(velocityAfter2.value), 700)
+    }
+
+    if (missingTerm == "velocityAfter1") {
+        ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), value, 100)
+        ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), parseFloat(velocityAfter2.value), 700)
+    } else if (missingTerm == "velocityAfter2") {
+        ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), parseFloat(velocityAfter1.value), 100)
+        ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), value, 700)
+    }
+
+    // If solved is undefined i.e. no value passed, then just generate the balls
+    if (value == undefined) {
+        ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), parseFloat(velocityAfter1.value), 100)
+        ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), parseFloat(velocityAfter2.value), 700)
+    }
+}
+
+let checkMissing = () => {
+    let missing = [];
+    if (mass1.value == "") {
+        missing.push("mass1");
+    }
+    if (mass2.value == "") {
+        missing.push("mass2");
+    }
+    if (velocity1.value == "") {
+        missing.push("velocity1")
+    }
+    if (velocity2.value == "") {
+        missing.push("velocity2")
+    }
+    if (velocityAfter1.value == "") {
+        missing.push("velocityAfter1")
+    }
+    if (velocityAfter2.value == "") {
+        missing.push("velocityAfter2")
+    }
+    console.log(missing)
+    return missing
+}
+
+let Start = () => {
+    // First check what is missing from the input
+    let missingStuffInArray = checkMissing(); // Returns what is missing, like what to solve for as an array
+    if (missingStuffInArray.length == 0) {
+        generateBalls(undefined)
+    } else if (missingStuffInArray.length > 1)  {
+        alert("Cannot solve, add more information")
+        return;
+    } else {
+        let solved = solveCollision(
+            parseFloat(mass1.value), 
+            parseFloat(mass2.value), 
+            parseFloat(velocity1.value), 
+            parseFloat(velocity2.value), 
+            parseFloat(velocityAfter1.value), 
+            parseFloat(velocityAfter2.value), 
+            missingStuffInArray[0])
+        generateBalls(solved, missingStuffInArray[0]);
+    }
+
+}
+
 // Start Animation once clicked
 StartButton.addEventListener("click", () => {
     // First reset everything
     ctx.clearRect(0, 0, 800, 400);
-    if (!isRunning) {
-        // ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), parseFloat(velocityAfter1.value), 100)
-        // ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), parseFloat(velocityAfter2.value), 700)
-        // TODO: Solve collision first, then return the value then create the ball
-        let missingTerm = solveCollision(parseFloat(mass1.value), parseFloat(mass2.value), parseFloat(velocity1.value), parseFloat(velocity2.value), parseFloat(velocityAfter1.value), parseFloat(velocityAfter2.value))
+    if (isRunning == false) {
+        Start()
+        // Start animation
         requestAnimationFrame(mainLoop)
         isRunning = true;
     } else {
-        // Just redeclare the variables
-        // ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), 100)
-        // ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), 700)
-        ball1 = new Ball(parseFloat(mass1.value), parseFloat(velocity1.value), parseFloat(velocityAfter1.value), 100)
-        ball2 = new Ball(parseFloat(mass2.value), parseFloat(velocity2.value), parseFloat(velocityAfter2.value), 700)
+        // Restart balls, and their values
+        Start()
     }
+    // isRunning ? Start(false) : Start(true)
 })
 
 
